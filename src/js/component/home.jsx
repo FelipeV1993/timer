2db777da5201e55a682743a382654f26263101a4
felipe <f.vasquezossa@gmail.com>
2022-07-05 17:17:10 -0400

    timer

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,22 +4,21 @@ import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
+const Home = (props) => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<div className="container-fluid p-0">
+    <div className="seconds-counter bg-black d-flex justify-content-center align-items-center text-light">
+	<i class="fa-solid fa-clock"></i>
+      <div >
+        <span className="border border-white mx-1">{(props.sexto==null?0:props.sexto)}</span>
+        <span className="border border-white mx-1">{props.quinto==null?0:props.quinto}</span>
+        <span className="border border-white mx-1">{props.cuarto==null?0:props.cuarto}</span>
+        <span className="border border-white mx-1">{props.tercero==null?0:props.tercero}</span>
+        <span className="border border-white mx-1">{props.segundo==null?0:props.segundo}</span>
+        <span className="border border-white mx-1">{props.primero==null?0:props.primero}</span>
+      </div>
+    </div>
+  </div>
 	);
 };
 
--- src/js/index.js
+++ src/js/index.js
@@ -7,6 +7,20 @@ import "../styles/index.css";
 
 //import your own components
 import Home from "./component/home.jsx";
+let counter=0
+function aumentarContador(){
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+console.log(counter.toString()[counter.toString().length - 6])
+counter++
+
+ReactDOM.render(<Home 
+    primero={counter.toString()[counter.toString().length - 1]}
+    segundo={counter.toString()[counter.toString().length - 2]}
+    tercero={counter.toString()[counter.toString().length - 3]}
+    cuarto={counter.toString()[counter.toString().length - 4]}
+    quinto={counter.toString()[counter.toString().length - 5]}
+    sexto={counter.toString()[counter.toString().length - 6]}
+    />, document.querySelector("#app"));
+}
+setInterval(aumentarContador,1)
+
